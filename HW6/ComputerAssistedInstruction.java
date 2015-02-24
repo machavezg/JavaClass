a581e9ce2c9eb6b929cd8152b20989a14f3f906b
Miguel <machavezg@icloud.com>
2015-02-23 18:44:24 -0800

    Update and rename HW6 to HW6/ComputerAssistedInstruction.java

---
+++ HW6/ComputerAssistedInstruction.java
@@ -0,0 +1,34 @@
+// Exercise 6.36 ComputerAssistedInstruction.java
+// Program generates single-digit arithmatic problems
+import java.util.Random;
+
+public class ComputerAssistedInstruction
+{
+   // Declare variables
+         
+   // create question and stores the corresponding correct answer
+   public void createQuestion()
+   {
+      // get two random numbers between 0 and 9
+      // get random number between 0 and 3 to get arithmatic operator (-,+,*,/)
+      
+      //use switch statement to create question and compute answer
+      //ouput question to the user
+
+    } // end method createQuestion
+   
+   // checks if the user answered correctly
+   public void checkResponse(int guess)
+   {
+            
+   } // end method checkResponse   
+
+   // create a new response
+   public String createResponse(boolean correct)
+   {
+      
+         
+   } // end method createResponse
+} // end class ComputerAssistedInstruction
+
+
