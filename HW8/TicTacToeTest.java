d519168a8b27fc87b622a88f63fb3e22167a8b2f
Miguel <machavezg@icloud.com>
2015-03-29 23:36:41 -0700

    Create TicTacToeTest.java

---
+++ HW8/TicTacToeTest.java
@@ -0,0 +1,12 @@
+// Exercise 8.17 TicTacToeTest.java
+// Play a game of Tic Tac Toe
+
+public class TicTacToeTest
+{
+   public static void main(String[] args)
+   {
+      TicTacToe game = new TicTacToe();
+      game.printBoard();
+      game.makeMove();
+   } 
+} // end class TicTacToeTest
