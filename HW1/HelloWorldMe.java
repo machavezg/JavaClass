27e994030ffaf7febe6b8ab1dfdfbeb4476437ba
Miguel <machavezg@icloud.com>
2015-03-29 21:10:20 -0700

    Create HelloWorldMe.java

---
+++ HW1/HelloWorldMe.java
@@ -0,0 +1,11 @@
+//Miguel A. Chavez ID# 2037320 CIS 182
+//January 16, 2015
+
+class HelloWorldMe {
+	
+	public static void main(String args[]) {
+		System.out.print("Hello World!\n");
+		System.out.print("My name is Miguel A. Chavez, my educational goal is ");
+		System.out.print("to obtain a Certification in Programming.\n");
+	}
+}
