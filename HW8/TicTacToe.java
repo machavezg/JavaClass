ebe2f47f7876acb6328cad5754ee3dcd751ef675
Miguel <machavezg@icloud.com>
2015-03-30 23:48:23 -0700

    Update TicTacToe.java

--- HW8/TicTacToe.java
+++ HW8/TicTacToe.java
@@ -1,5 +1,8 @@
 /*
-
+Miguel A. Chavez
+ID 2037320
+CIS 182
+LAB 9
  */
 import java.util.Scanner;
 
@@ -8,6 +11,11 @@ public class TicTacToe {
     private final int win = 1, draw = 2, cont = 3;
     private int board[][];
     private boolean firstPlayer, gameOver;
+    //Assign values to enum
+    private static final int EMPTY = 0;
+    private static final int X = 1;
+    private static final int O = 2;
+    
     
     public TicTacToe()
     {
@@ -15,6 +23,9 @@ public class TicTacToe {
     firstPlayer = true;
     gameOver = false;
     }
+    
+    //Begin enum for empty, player 1 or player 2
+    public enum status{ EMPTY, X, O }
     
     public void makeMove()
     {
@@ -37,7 +48,7 @@ public class TicTacToe {
                 }
                 
                 firstPlayer = false;
-                board[row][column] = 1;
+                board[row][column] = X;
                 printBoard();
                 
                 printStatus(1);
@@ -58,7 +69,7 @@ public class TicTacToe {
                 }
                 
                 firstPlayer = true;
-                board[row][column] = 2;
+                board[row][column] = O;
                 printBoard();
                 
                 printStatus(2);
@@ -95,26 +106,26 @@ public class TicTacToe {
     {
         int a;
         //Check for a win on diagonals
-        if ( board[ 0 ][ 0 ] != 0 && board[ 0 ][ 0 ] == board[ 1 ][ 1 ] && 
+        if ( board[ 0 ][ 0 ] != EMPTY && board[ 0 ][ 0 ] == board[ 1 ][ 1 ] && 
             board[ 0 ][ 0 ] == board[ 2 ][ 2 ] )
             return win;
-        else if ( board[ 2 ][ 0 ] != 0 && board[ 2 ][ 0 ] == 
+        else if ( board[ 2 ][ 0 ] != EMPTY && board[ 2 ][ 0 ] == 
             board[ 1 ][ 1 ] && board[ 2 ][ 0 ] == board[ 0 ][ 2 ] )
             return win;
         //Check for win in rows
         for ( a = 0; a < 3; ++a )
-            if ( board[ a ][ 0 ] != 0 && board[ a ][ 0 ] ==
+            if ( board[ a ][ 0 ] != EMPTY && board[ a ][ 0 ] ==
                 board[ a ][ 1 ] && board[ a ][ 0 ] == board[ a ][ 2 ] )
                 return win;
         //Check for win in columns
         for ( a = 0; a < 3; ++a )
-            if ( board[ 0 ][ a ] != 0 && board[ 0 ][ a ] ==
+            if ( board[ 0 ][ a ] != EMPTY && board[ 0 ][ a ] ==
                 board[ 1 ][ a ] && board[ 0 ][ a ] == board[ 2 ][ a ] )
                 return win;
         //Check for a completed game
         for ( int r = 0; r < 3; ++r )
             for ( int c = 0; c < 3; ++c )
-                if ( board[ r ][ c ] == 0 )
+                if ( board[ r ][ c ] == EMPTY )
                     return cont;//Game is not finished
         return draw; //Game is a draw
     }//End gameStatus
@@ -140,26 +151,26 @@ public class TicTacToe {
         
         if(column != 2) {//First two columns
             switch(player){
-                case 0:
+                case EMPTY:
                     output = "|       ";
                     break;
-                case 1:
+                case X:
                     output = "|   X   ";
                     break;
-                case 2:
+                case O:
                     output = "|   O   ";
                     break;
             }
         }
         else{//Last column
             switch(player){
-                case 0:
+                case EMPTY:
                     output = "|       |\n";
                     break;
-                case 1:
+                case X:
                     output = "|   X   |\n";
                     break;
-                case 2:
+                case O:
                     output = "|   O   |\n";
                     break;
             }
@@ -171,6 +182,6 @@ public class TicTacToe {
     public boolean validMove(int row, int column)
     {
         return row >= 0 && row < 3 && column >= 0 && column < 3 &&
-            board[row][column] == 0;
+            board[row][column] == EMPTY;
     }
 }
