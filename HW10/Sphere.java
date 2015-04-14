707f4e43593c9469bd22567d03940da17332abb9
Miguel <machavezg@icloud.com>
2015-04-13 19:07:26 -0700

    Update Sphere.java

--- HW10/Sphere.java
+++ HW10/Sphere.java
@@ -14,17 +14,12 @@ public class Sphere extends ThreeDimensionalShape {
   
   public int getArea()
   {
-    return (int)( 4 * Math.PI * super.getDimension1() * super.getDimension1() );
+    return (int)( 4 * Math.PI * super.getDimension1() * super.getDimension2() );
   }
   
   public int getVolume()
   {
-    return (int)( ( 4 / 3 ) * Math.PI * super.getDimension1() * super.getDimension1() * super.getDimension1() );
-  }
-  
-  public void print()
-  {
-    System.out.println( "(" + super.getX() + ", " + super.getY() + ") " + "radius: " + super.getDimension1() );
+    return (int)( ( 4 * Math.PI * super.getDimension1() * super.getDimension2() * super.getDimension3() ) / 3 );
   }
   
 }
