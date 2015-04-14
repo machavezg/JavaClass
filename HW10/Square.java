45777ff9a7987c731f089bcd876d5a92df7121b0
Miguel <machavezg@icloud.com>
2015-04-13 18:15:14 -0700

    Update Square.java

--- HW10/Square.java
+++ HW10/Square.java
@@ -1,1 +1,34 @@
+// Square.java
 
+public class Square extends TwoDimensionalShape {
+  
+  public Square (int x, int y, int side ) 
+  {
+    super ( x, y, side, side );
+  }
+  
+  public String getName() 
+  {
+    return "Square";
+  }
+  
+  public void print()
+  {
+    System.out.println( "(" + super.getX() + ", " + super.getY() + ") " + "side: " + super.getDimension1() );
+  }
+  
+  public int area()
+  {
+    return super.getDimension1() * super.getDimension1();
+  }
+  
+  public void setRadius( int side )
+  {
+    super.setDimension1( side );
+  }
+  
+  public int getSide()
+  {
+    return super.getDimension1();
+  }
+}
