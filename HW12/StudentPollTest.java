21757110724e198e50aa532689195e4a07a8fc81
Miguel <machavezg@icloud.com>
2015-04-27 15:14:53 -0700

    Create StudentPollTest.java

---
+++ HW12/StudentPollTest.java
@@ -0,0 +1,12 @@
+// StudentPollTest.java
+
+
+public class StudentPollTest
+{
+   public static void main( String args[] )
+   {
+      StudentPoll application = new StudentPoll();
+      application.displayData();
+   } // end main
+} // end class StudentPollTest
+
