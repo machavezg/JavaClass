4580d5589e6ad1056a4b9c6c8193737d1b48cff6
Miguel <machavezg@icloud.com>
2015-05-02 14:17:14 -0700

    Create FileDemonstrationTest.java

---
+++ HW13/FileDemonstrationTest.java
@@ -0,0 +1,13 @@
+// Fig. 17.13: FileDemonstrationTest.java
+// Testing class FileDemonstration.
+import javax.swing.JFrame;
+
+public class FileDemonstrationTest
+{
+   public static void main( String[] args )
+   {
+      FileDemonstration application = new FileDemonstration();
+      application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+   } // end main
+} // end class FileDemonstrationTest
+
